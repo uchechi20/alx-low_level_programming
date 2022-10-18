fd30c0c3e78349b9f1bfa6a0701aa3c406a15d3e
uchechi20 <chinenyeuchechukwu20@gmail.com>
2022-10-18 00:42:09 -0700

    function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0-putchar.c
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ * Author: Genuis Villagers
+ * Desc: Header contains protypes for all funtions
+ * used in 0x02-functions_nested_loops directory
+ */
+
+int _putchar(char c);
+int _isdigit(int c);
+
+
+
+
+
+
+
+
+
+#endif

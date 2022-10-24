c330ea121bfe56c587a53918c726502b5d3eaf89
uchechi20 <chinenyeuchechukwu20@gmail.com>
2022-10-24 02:16:01 -0700

    task 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,14 +8,20 @@
  * Return: Always 0
  */
 void swap_int(int *a, int *b);
-{
-	int a;
-	int b;
+int a;
+int b;
 
+	{
 	a = 98;
 	b = 42;
+	}
+	{
 	printf("a = %d, b = %d\n", a, b);
-	swap_int(&a, &b);
+		swap_int(&a, &b);
+	}
+	{
 	printf("a = %d, b = %d\n", a, b);
-	return (0);
-}
+	}
+
+return (0);
+

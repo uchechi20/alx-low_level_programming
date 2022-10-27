801ad7fc9e5e2674aa84cf71a833583b64059db3
uchechi20 <chinenyeuchechukwu20@gmail.com>
2022-10-27 08:52:41 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,11 +8,27 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+
 	while (str[i])
 	{
-		while (!(str[1] >= 'a' && str[1] <= 'z'
-))
+		while (!(str[1] >= 'a' && str[1] <= 'z'))
+			i++;
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
 		    str[i - 1] == '\n' ||
 		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
+		    i == 0;)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}

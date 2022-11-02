4c40eff1ffc589f256130801f5d0ce2b1a24d1c8
uchechi20 <chinenyeuchechukwu20@gmail.com>
2022-11-02 07:41:28 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print a string
- * @s: pointer block of memory to fill
- * Return: void
+ * _puts_recursion - print a string followed by a new line
+ * @s: string
+ *
+ * Return: On success 1
+ * On error, -1 is returned and error is set appropriately
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == 0)
 	{
 		_putchar('\n');
 		return;
